d11226189e1b65e1cc8435f8b62ac4b746b2f1e4
Kunihiko Ohnaka <kuni@ohnaka.jp>
2022-12-22 06:54:13 +0900

    Fix file name miss.

--- RTL/sources.mk
+++ RTL/sources.mk
@@ -3,7 +3,7 @@ SOURCES =  X68KeplerX_pkg.vhd lib/i2c/I2C_pkg.vhd \
 	adpcm/e6258.vhd adpcm/calcadpcm.vhd \
   	peripheral/e8255.vhd mercury/eMercury.vhd midi/em3802.vhd \
 	lib/i2s/i2s_encoder.vhd lib/i2s/i2s_decoder.vhd \
-	lib/addsat.vhd lib/datfifo.vhd lib/txframe.vhd lib/rxframe.vhd lib/sftclk.vhd \
+	lib/addsat/addsat.vhd lib/addsat/addsat_16.vhd lib/datfifo.vhd lib/txframe.vhd lib/rxframe.vhd lib/sftclk.vhd \
 	lib/i2c/I2CIF.vhd \
 	lib/wm8804/wm8804.vhd \
 	exmemory/exmemory.vhd \
